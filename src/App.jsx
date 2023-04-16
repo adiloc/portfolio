57eeaec14c464d13a577d8e545e8f17f2781e7a3
Adrian <adriandumitrascu.ro@gmail.com>
2023-04-17 00:45:08 +0200

    three js add plane

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Canvas } from "@react-three/fiber";
+
+
+import './style.css'
+
+function App() {
+
+
+  return (
+
+    <div className="container">
+      <h1>
+        Home
+        </h1>
+    </div>
+    
+  )
+}
+
+export default App
